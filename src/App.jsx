d494513f7873f11a3b97f99ffa97d8800a3820b6
Conor <63920243+Conjons7@users.noreply.github.com>
2020-06-13 11:08:25 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+// import GoogleApiWrapper from './GoogleApiWrapper';
+
+class App extends Component {
+
+    
+
+  render() {
+ 
+
+    return (
+        <div className='container'>
+            <div className='Welcome'>
+                <h1>Sweet San Diego Sunsets</h1>
+                <h2>Welcome to our website!</h2>
+            </div>
+            <div id="map">
+              
+            </div>
+        </div>
+    );
+  }
+}
+
+
+export default App;
